391758883cce6e8c0fed719a5d37b5244a5376ee
devgunho <devgun@kakao.com>
2021-10-06 05:57:57 +0900

    add: pysftp sample code

--- Automation Code/automated_controller.py
+++ Automation Code/automated_controller.py
@@ -1,3 +1,4 @@
+import pysftp
 import csv
 import os
 import paramiko
@@ -55,5 +56,26 @@ if __name__ == "__main__":
     target_ec2 = get_aws_ec2_info()
     aws_connect(target_ec2)
 
-    # To Do List
-    # sftp
+
+# To Do List
+# sftp
+# _______________________________________
+# import pysftp
+
+# myHostname = "yourserverdomainorip.com"
+# myUsername = "root"
+# myPassword = "12345"
+
+# with pysftp.Connection(host=myHostname, username=myUsername, password=myPassword) as sftp:
+#     print "Connection succesfully stablished ... "
+
+#     # Define the file that you want to upload from your local directorty
+#     # or absolute "C:\Users\sdkca\Desktop\TUTORIAL2.txt"
+#     localFilePath = './TUTORIAL2.txt'
+
+#     # Define the remote path where the file will be uploaded
+#     remoteFilePath = '/var/integraweb-db-backups/TUTORIAL2.txt'
+
+#     sftp.put(localFilePath, remoteFilePath)
+
+# # connection closed automatically at the end of the with-block
